a772680ceb9c13da347d1a205e848ebbfc07e7a9
dmitriy mukhanov <74504728+mityamuch@users.noreply.github.com>
2023-04-27 21:13:31 +0300

    small update

--- main.py
+++ main.py
@@ -140,6 +140,61 @@ def makeSimpleCityTravelTicketEn(usr):
             os.path.join(os.getcwd(), 'personal', usr["name"], filename))
 
 
+def makeExchangeCityTravelticketEn(usr):
+    print(f'\r[+] Заполняю билет : {usr["name"]}', end='')
+
+    data = {'tickN': usr["tickN"],
+            'aviaN': usr["aviaN"],
+            'orderN': usr["orderN"],
+            'name': usr["name"],
+            'bornDate': usr["bornDate"],
+            'passportN': usr["passportN"],
+            'timeFly': usr["timefly"],
+            'planeN': usr["planeN"],
+            'aviaComp': usr["aviaComp"],
+            'planeType': usr["planeType"],
+            'baggage': usr["baggage"],
+            'outTime': usr["outTime"],
+            'outDate': usr["outDate"],
+            'regTime': usr["regtime"],
+            'portNameS': usr["portNameS"],
+            'city': usr["city"],
+            'coutry': usr["country"],
+            'portName': usr["portName"],
+            'inTime': usr["inTime"],
+            'inDate': usr['inDate'],
+            'distance': usr['distance'],
+            'orderDate': usr["orderDate"],
+            'orderTime': usr['orderTime'],
+            'sum': usr["sum"],
+            'val': usr['val'],
+            'planeN2': usr['planeN2'],
+            'aviaComp2': usr['aviaComp2'],
+            'planeType2': usr['planeType2'],
+            'outDate2': usr['outDate2'],
+            'outTime2': usr['outTime2'],
+            'regTime2': usr['regTime2'],
+            'portNameS2': usr['portNameS2'],
+            'city2': usr['city2'],
+            'coutry2': usr['coutry2'],
+            'portName2': usr['portName2'],
+            'inTime2': usr['inTime2'],
+            'inDate2': usr['inDate2'],
+            'distance2': usr['distance2'],
+            'transferType': usr['transferType'],
+            'portTransferName': usr['portTransferName'],
+            'timeTransfer': usr['timeTransfer'],
+            'aviaPhone': usr['aviaPhone'],
+            }
+    docticket = DocxTemplate("templateCityTravel2city.docx")
+    docticket.render(data)
+    docticket.save(os.path.join(os.getcwd(), 'personal', usr["name"], f'{usr["name"]}.docx'))
+    date = (usr["orderDate"] + usr["orderTime"]).replace(".", "").replace(':', '')
+    filename = f'{usr["orderN"]}_{date}{random.randint(1000000, 9999999)}forward_aerovaucher.pdf'
+    convert(os.path.join(os.getcwd(), 'personal', usr["name"], f'{usr["name"]}.docx'),
+            os.path.join(os.getcwd(), 'personal', usr["name"], filename))
+
+
 def makePolicy(usr):
     print(f'\r[+] Заполняю страховку : {usr["name"]}', end='')
 
@@ -217,8 +272,7 @@ def filling_docs():
 
     print("1 - Билет на самолет")
     print("2 - Страховка")
-    print("3 - Бронирование отеля")
-    print("4 - Весь пакет документов")
+    print("3 - Весь пакет документов")
 
     choice = input("Введите номер выбранного типа документов: ")
     if choice == "1":
@@ -233,7 +287,7 @@ def filling_docs():
                 makeSimpleCityTravelTicketEn(usr)
             else:
                 print("Создаю билет с пересадкой")
-                #makeExchangeCityTravelticket(usr)
+                # makeExchangeCityTravelticket(usr)
         elif lang == "ru":
             if usr["planeN2"] == "":
                 print("Создаю билет без пересадки")
@@ -253,7 +307,7 @@ def filling_docs():
         print("Выбран тип документов 3")
         makeBooking(usr)
 
-    elif choice == "4":
+    elif choice == "3":
         print("Выбран тип документов 3")
         if usr["planeN2"] == "":
             print("Создаю билет без пересадки")
